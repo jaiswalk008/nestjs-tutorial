b23094d81102673fa8ca7fc0c5d2394692c750f7
Karan Jaiswal <jaiskaran008@gmail.com>
2024-05-29 17:28:14 +0530

    added services file

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,40 +1,39 @@
 import {Body, Controller , Delete, Get, HttpCode, Param, Patch, Post, Query} from '@nestjs/common';
-import { UserService } from './user.service';
+import { UserService,UserInterface } from './user.service';
 
 @Controller('user') // route: /user
 export class UserController{
     constructor(private readonly userService:UserService){}
      
     @Get()
-    findAll(@Query('role') role?: 'Intern' | 'Engineer' | 'Admin' , 
-        @Query('age') age?: 18){
-        return {role ,age};
+    findAll(@Query('role') role?: 'Intern' | 'Engineer' | 'Admin'){
+        console.log(role);
+        return this.userService.getUsers(role);
     }
-    @Get('intern') // GET /user/intern
-    findAllInterns(){
-        return []
-    }
+    // @Get('intern') // GET /user/intern
+    // findAllInterns(){
+    //     return []
+    // }
     
     @Get(':id') // /user/:id
-    findOne(@Param('id') id:string){ // GET /user/1
+    findOne(@Param('id') id:number){ // GET /user/1
         // return this.userService.getUser();
-        return {id};
+        return this.userService.findOne(+id);
     }
     @Post() //POST /user
-    createUser(@Body() user:{}){
-        return user
+    createUser(@Body() user:UserInterface){
+        return this.userService.createUser(user);
     }
     
     @Patch(':id') // PATCH /user/:id/
-    updateUser(@Param('id') id:string, @Body() user:{}){
-        return {id,...user};
+    updateUser(@Param('id') id:number, @Body() user:UserInterface){
+        return this.userService.updateUser(+id,user);
     }
     
     @Delete(':id')
-    @HttpCode(204)
-    deleteUser(@Param('id') id:string) {
-        return {id};
-    
+    @HttpCode(201)
+    deleteUser(@Param('id') id:number) {
+        return this.userService.deleteUser(+id);
     }
 
 }
--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,9 +1,55 @@
 import { Injectable } from "@nestjs/common";
-
+export interface UserInterface {
+    id: number;
+    name: string;
+    email: string;
+    role: string;
+  }
 @Injectable()
 export class UserService{
-    getUsers():string {
-        return "Hello user";
+    private users: UserInterface[] = [
+        { id: 1, name: 'Alice Johnson', email: 'alice.johnson@example.com', role: 'Engineer' },
+        { id: 2, name: 'Bob Smith', email: 'bob.smith@example.com', role: 'Intern' },
+        { id: 3, name: 'Charlie Brown', email: 'charlie.brown@example.com', role: 'Admin' },
+        { id: 4, name: 'David Wilson', email: 'david.wilson@example.com', role: 'Engineer' },
+        { id: 5, name: 'Eva Green', email: 'eva.green@example.com', role: 'Intern' },
+        { id: 6, name: 'Frank White', email: 'frank.white@example.com', role: 'Admin' },
+        { id: 7, name: 'Grace Black', email: 'grace.black@example.com', role: 'Engineer' },
+        { id: 8, name: 'Henry Yellow', email: 'henry.yellow@example.com', role: 'Intern' },
+        { id: 9, name: 'Ivy Blue', email: 'ivy.blue@example.com', role: 'Admin' },
+        { id: 10, name: 'Jack Red', email: 'jack.red@example.com', role: 'Engineer' },
+      ];
+    getUsers(role?: 'Intern'| 'Admin' | 'Engineer'){
+        
+        if(role){
+            return this.users.filter(user => user.role === role);
+        }
+        return this.users;
     }
-}
+    findOne(id:number){
+        const res =  this.users.filter(user => user.id === id)[0];
+        console.log(res);
+        return res;
+    }
+    createUser(user:UserInterface){
+        const id = this.users.length+1;
+        this.users.push({id, ...user});
+        return {id,...user};
+    }
+    updateUser(id :number, userData:UserInterface){
+        let updatedUser={};
+        this.users = this.users.map((user:UserInterface) =>{
+            if(user.id === id){
+                updatedUser = {...user, ...userData};
+                return {...user, ...userData};
+            }
+            return user;
+        })
+        return updatedUser
+    }
+    deleteUser(id:number){
+        this.users = this.users.filter(user => user.id !== id);
+        return {message: 'User deleted successfully.'}
+    }
 
+}
